3ebbe02771b806ea7dbd7cc43c0b4d14aaad192f
Laptop_Bogdan <bogdanvoinov90@gmail.com>
2024-11-09 10:12:45 +0300

    update main

--- kurs/main.py
+++ kurs/main.py
@@ -1,12 +1,15 @@
+import sys
+
 from lexer import lexer
 from syntax import syntax
 
 if __name__ == '__main__':
     try:
-        for i, line in enumerate(open("kurs/prog.txt")):
+        filename = "kurs/prog.txt"
+        for i, line in enumerate(open(filename)):
             print(str(i)+":", line, end="")
         print()
-        tokens_and_table = lexer('kurs/prog.txt')
+        tokens_and_table = lexer(filename)
         # print(*tokens_and_table, sep='\n')
         syntax(tokens_and_table[1], tokens_and_table[0])
         print("Ошибки не обнаружены")
